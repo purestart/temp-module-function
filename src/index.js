f9a580a0a326ed843b326627b826abe7a1375fee
purestart <522382456@qq.com>
2019-08-23 15:24:54 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {{options.folder}}Store from './vuex/'
 
-import {{options.folder}}RegComponents from '{{options.folder}}src/util/{{options.folder}}RegComponents'
-import {{options.folder}}Routes from '{{options.folder}}src/router/'
+import {{options.folder}}RegComponents from '{{options.folder}}Src/util/{{options.folder}}RegComponents'
+import {{options.folder}}Routes from '{{options.folder}}Src/router/'
 
 export { {{options.folder}}Routes, {{options.folder}}Store, {{options.folder}}RegComponents }
